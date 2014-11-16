10b682b25cbfc9bec7593a216b188d941584300d
bttalic <b.ttalic@gmail.com>
2014-11-16 12:54:32 +0100

    Interval?

--- src/zadaca/novembar152014/IspisBrojeva.java
+++ src/zadaca/novembar152014/IspisBrojeva.java
@@ -7,6 +7,7 @@ public class IspisBrojeva {
 		 * program ce ispisivati brojeve izmedju 10 i 100 hiljada ukljucujuci i
 		 * te brojeve, unazad i svaki broj posebno u jednoj liniji koda.
 		 * samo komentar da se vidi da je nesto promjenjeno
+		 * Hoce li 100 000 biti ukljucen ? I hoce li program raditi za taj broj ?
 		 */
 		System.out.println("Inicijalni broj:  " + brojcicNeki);
 		System.out.println("Zadnja cifra je:  " + brojcicNeki % 10);
